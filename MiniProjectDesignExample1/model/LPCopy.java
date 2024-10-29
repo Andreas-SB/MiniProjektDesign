cb6782b7abec8425c45ad5eda7b6f78eea5294d9
Andreas-SB <10632087@ucn.dk>
2024-10-29 11:49:07 +0100

    Update LPCopy.java

--- MiniProjectDesignExample1/model/LPCopy.java
+++ MiniProjectDesignExample1/model/LPCopy.java
@@ -12,18 +12,18 @@ public class LPCopy
     // instance variables - replace the example below with your own
     private String serialNumber;
     private String purchaseDate;
-    private int purcahsePrice;
+    private int purchasePrice;
     private String condition;
 
     /**
      * Constructor for objects of class LPCopy
      */
-    public LPCopy(String serialNumber,String purchaseDate,int purcahsePrice,String condition)
+    public LPCopy(String serialNumber,String purchaseDate,int purchasePrice,String condition)
     {
         // initialise instance variables
         this.serialNumber = serialNumber;
         this.purchaseDate = purchaseDate;
-        this.purcahsePrice = purcahsePrice;
+        this.purchasePrice = purchasePrice;
         this.condition = condition;
     }
 
