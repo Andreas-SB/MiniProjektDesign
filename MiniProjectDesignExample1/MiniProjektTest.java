2c18e3708bb78a65dc6de22397c00dca95469b00
Andreas-SB <10632087@ucn.dk>
2024-10-31 14:53:36 +0100

    Test layout

---
+++ MiniProjectDesignExample1/MiniProjektTest.java
@@ -0,0 +1,51 @@
+
+
+import static org.junit.jupiter.api.Assertions.*;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.util.ArrayList;
+
+/**
+ * The test class MiniProjektTest.
+ *
+ * @author  (your name)
+ * @version (a version number or a date)
+ */
+public class MiniProjektTest
+{
+    /**
+     * Default constructor for test class MiniProjektTest
+     */
+    public MiniProjektTest()
+    {
+    }
+
+    /**
+     * Sets up the test fixture.
+     *
+     * Called before every test case method.
+     */
+    @BeforeEach
+    public void setUp()
+    {
+    }
+
+    /**
+     * Tears down the test fixture.
+     *
+     * Called after every test case method.
+     */
+    @AfterEach
+    public void tearDown()
+    {
+    }
+    @Test
+    public void testCarModel(){
+        //Arrange
+        //Act
+        
+        //Assert
+   
+    }
+}
